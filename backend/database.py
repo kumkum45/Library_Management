7a0ac40f9e7012c72addd781cf547be9d02b28da
Kumkum Solanki <kumkumsolanki65@gmail.com>
2025-12-22 16:01:07 +0530

    Addedd Supabase Url

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine,Column, Integer, String,ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base , relationship
 
 # databse url
-db_url="postgresql://postgres:root@localhost:5432/Library_db"
+db_url="postgresql://postgres:supabase!01@db.dzikcpihkcdsblffgldy.supabase.co:5432/postgres"
 
 engine = create_engine(db_url)
 Base=declarative_base()
+
 
